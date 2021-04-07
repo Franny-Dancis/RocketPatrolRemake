4fe141fec52bde3943732b37222e38a3dca4c272
Franny-Dancis <42708672+Franny-Dancis@users.noreply.github.com>
2021-04-07 14:24:55 -0700

    Added starfield png

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -2,7 +2,18 @@ class Play extends Phaser.Scene{
     constructor(){
         super("playScene");
     }
+
+    preload() {
+        this.load.image("starfield", "assets/starfield.png");
+    }
+
+
     create(){
+        // starfield
+        this.starfield = this.add.tileSprite(
+            0,0,640,480, 'starfield', 
+        ).setOrigin(0,0);
+        
         // green UI background
         this.add.rectangle(
             0, 
@@ -17,6 +28,5 @@ class Play extends Phaser.Scene{
 	    this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0 ,0);
 	    this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
 	    this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
-
     }
 }
