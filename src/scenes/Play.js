940b3e42cb8749203982a54472429866f948a55c
Franny-Dancis <42708672+Franny-Dancis@users.noreply.github.com>
2021-04-09 14:11:51 -0700

    Enemy ships wrapping around

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -6,6 +6,7 @@ class Play extends Phaser.Scene{
     preload() {
         this.load.image("starfield", "assets/starfield.png");
         this.load.image("rocket", "assets/rocket.png");
+        this.load.image("spaceship", "assets/spaceship.png")
     }
 
     create(){
@@ -21,6 +22,29 @@ class Play extends Phaser.Scene{
             game.config.height - borderUISize - borderPadding,
             'rocket'
         );
+
+        // spaceship objects
+        this.ship1 = new Ship(
+            this,
+            100,
+            200,
+            'spaceship'
+        )
+        
+        // spaceship objects
+        this.ship2 = new Ship(
+            this,
+            300,
+            240,
+            'spaceship'
+        )
+         // spaceship objects
+        this.ship3 = new Ship(
+            this,
+            380,
+            150,
+            'spaceship'
+        )
 
         // green UI background
         this.add.rectangle(
@@ -46,7 +70,11 @@ class Play extends Phaser.Scene{
     }    
     update() {
         this.starfield.tilePositionX += 4;
-        
+
         this.p1Rocket.update();
+
+        this.ship1.update();
+        this.ship2.update();
+        this.ship3.update();
         }
 }
