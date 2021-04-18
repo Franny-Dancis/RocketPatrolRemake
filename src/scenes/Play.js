232ac49d7d565e1822ac22157b05263ac27f57d0
Franny-Dancis <42708672+Franny-Dancis@users.noreply.github.com>
2021-04-18 12:24:59 -0700

    Added ./ to file paths

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -4,10 +4,10 @@ class Play extends Phaser.Scene{
     }
 
     preload() {
-        this.load.image("starfield", "assets/starfield.png");
-        this.load.image("paperBackground", "assets/paperBackground.png");
-        this.load.image("rocket", "assets/rocket.png");
-        this.load.image("spaceship", "assets/spaceship.png");
+        this.load.image("starfield", "./assets/starfield.png");
+        this.load.image("paperBackground", "./assets/paperBackground.png");
+        this.load.image("rocket", "./assets/rocket.png");
+        this.load.image("spaceship", "./assets/spaceship.png");
         this.load.spritesheet('explosion', './assets/explosion.png', {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 9});
     }
 
