b43aeb8d9f259a4318cde774da54839b94717704
Franny-Dancis <42708672+Franny-Dancis@users.noreply.github.com>
2021-04-18 12:03:02 -0700

    Entered Last changes

--- src/main.js
+++ src/main.js
@@ -13,3 +13,24 @@ let borderPadding = borderUISize / 3;
 
 // Added keyDOWN for instructions
 let keyLEFT, keyRIGHT, keyDOWN, keyF, keyR, keyP;
+
+/*
+Name : Dany Francis
+Project Title: Rocket Patrol Mod
+Date: 4/18/2021
+Estimated Time to Complete: 11 Hours
+*/
+
+////////////////////////////////////////////////
+
+/*        POINT BREAKDOWN       \
+/                               \
+/      4 NEW SFX = 10 POINTS    \
+/ HIGH SCORE TRACKING = 5 POINTS\
+/ MOUSE CONTROLS = 20 POINTS    \
+/ NEW BACKGROUND = 5 POINTS     \
+/ ADD TIME MECHANIC = 20 POINTS \
+/ MY MUSIC = 5 POINTS           \
+/ CONTROL ROCKET AFTER FIRING = 5 POINTS     \
+/ INSTRUCTIONS SCREEN = HOPEFULLY SOME POINTS \
+*/
